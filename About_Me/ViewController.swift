00b5e185fc79f2a07e078c178a3c58d0bbc0afbc
Ronin619 <eytam619@gmail.com>
2023-04-20 21:28:28 -0400

    Initial Commit

---
+++ About_Me/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  About_Me
+//
+//  Created by Eric Tam on 4/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let imageContainerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius = 20
+        view.backgroundColor = .darkGray
+        return view
+    }()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        setImageContainerView()
+    }
+    
+    func setImageContainerView() {
+        view.addSubview(imageContainerView)
+        
+        NSLayoutConstraint.activate([
+            imageContainerView.topAnchor.constraint(equalTo: view.topAnchor, constant: 60),
+            imageContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            imageContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            imageContainerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.4),
+        ])
+    }
+}
+
