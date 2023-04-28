683ee0c4ce7573df4c4db951e7cf4411a0e72963
Ronin619 <eytam619@gmail.com>
2023-04-27 21:42:40 -0400

    feat: implement location and social media view

--- About_Me/ViewController.swift
+++ About_Me/ViewController.swift
@@ -16,42 +16,20 @@ class ViewController: UIViewController {
         return view
     }()
     
-    let profileImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = UIImage(named: "profile")
-        imageView.contentMode = .scaleAspectFit
-        imageView.frame = CGRect(x: 70, y: 20, width: 170, height: 170)
-        return imageView
+    let locationSocialMediaView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .blue
+        return view
     }()
     
-//    let profileImageView: UIImageView = {
-//        let imageView = UIImageView()
-//        imageView.image = UIImage(named: "profile")
-//        return imageView
-//    }()
     
-    let nameLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "Eric Tam"
-        return label
-    }()
-    
-    let pronounLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "he/him/his"
-        return label
-    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         
         setProfileContainerView()
-        setProfileImageView()
-        setNameLabel()
-        setPronounLabel()
     }
     
     func setProfileContainerView() {
@@ -62,37 +40,6 @@ class ViewController: UIViewController {
             profileContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
             profileContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
             profileContainerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.3),
-        ])
-    }
-    
-    func setProfileImageView() {
-        profileContainerView.addSubview(profileImageView)
-
-//        NSLayoutConstraint.activate([
-//            profileImageView.topAnchor.constraint(equalTo: profileContainerView.topAnchor, constant: 30),
-//            profileImageView.leadingAnchor.constraint(equalTo: profileContainerView.leadingAnchor, constant: 30),
-//            profileImageView.trailingAnchor.constraint(equalTo: profileContainerView.trailingAnchor, constant: -30),
-//            profileImageView.bottomAnchor.constraint(equalTo: profileContainerView.bottomAnchor, constant: -30),
-//        ])
-    }
-    
-    func setNameLabel() {
-        profileContainerView.addSubview(nameLabel)
-        
-        NSLayoutConstraint.activate([
-            nameLabel.topAnchor.constraint(equalTo: profileContainerView.topAnchor, constant: 180),
-            nameLabel.leadingAnchor.constraint(equalTo: profileContainerView.leadingAnchor, constant: 120),
-            nameLabel.bottomAnchor.constraint(equalTo: profileContainerView.bottomAnchor, constant: -30)
-        ])
-    }
-    
-    func setPronounLabel() {
-        profileContainerView.addSubview(pronounLabel)
-        
-        NSLayoutConstraint.activate([
-            pronounLabel.topAnchor.constraint(equalTo: profileContainerView.topAnchor, constant: 190),
-            pronounLabel.leadingAnchor.constraint(equalTo: profileContainerView.leadingAnchor, constant: 115),
-            pronounLabel.bottomAnchor.constraint(equalTo: profileContainerView.bottomAnchor, constant: -5)
         ])
     }
 }
